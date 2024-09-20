8c3af1c24d589572505954698c8d4d4d0c10b921
enzop <enzop@10.3.61.212>
2024-09-20 17:09:37 +0200

    Création d'un nouveau projet versionné

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package calculator;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +13,19 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    System.out.println("Please enter the operator:");
+    System.out.println("1) add");
+    System.out.println("2) substract");
+    System.out.println("3) multiply");
+    System.out.println("4) divide");
+    System.out.println("5) modulo");
     
-}
+    Scanner scanner = new Scanner(System.in);
+    int operateur = scanner.nextInt();
+    System.out.println("You selected operator: " + operateur);}
+    
+
+    
+    
+    
+
