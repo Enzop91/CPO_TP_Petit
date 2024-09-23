8d68c942f552366f2f66d5a9038644e0cb802863
enzop <enzop@10.3.61.212>
2024-09-23 11:50:46 +0200

    Création d'un nouveau projet versionné

--- TP1_manipNombresInt_PetitEnzo/src/tp1_manipnombresint_petitenzo/TP1_manipNombresInt_PetitEnzo.java
+++ TP1_manipNombresInt_PetitEnzo/src/tp1_manipnombresint_petitenzo/TP1_manipNombresInt_PetitEnzo.java
@@ -4,6 +4,8 @@
  */
 package tp1_manipnombresint_petitenzo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author enzop
@@ -14,7 +16,20 @@ public class TP1_manipNombresInt_PetitEnzo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+            Scanner scanner = new Scanner(System.in);
+        
+       
+        System.out.print("Veuillez entrer le premier entier : ");
+        int premierEntier = scanner.nextInt();
+        
+        System.out.print("Veuillez entrer le second entier : ");
+        int secondEntier = scanner.nextInt();
+        
+        System.out.println("Le premier entier saisi est : " + premierEntier);
+        System.out.println("Le second entier saisi est : " + secondEntier);
+        
+        scanner.close();
     }
-    
 }
+        
