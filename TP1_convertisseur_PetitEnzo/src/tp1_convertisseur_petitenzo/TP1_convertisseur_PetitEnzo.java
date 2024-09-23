8e1cffa319f5762699eaca3f58aa6f7f04f0b1a9
enzop <enzop@10.3.61.212>
2024-09-23 12:54:24 +0200

    Création d'un nouveau projet versionné

--- TP1_convertisseur_PetitEnzo/src/tp1_convertisseur_petitenzo/TP1_convertisseur_PetitEnzo.java
+++ TP1_convertisseur_PetitEnzo/src/tp1_convertisseur_petitenzo/TP1_convertisseur_PetitEnzo.java
@@ -12,6 +12,10 @@ import java.util.Scanner;
  */
 public class TP1_convertisseur_PetitEnzo {
 
+     public static double CelciusVersKelvin(double tCelsius) {
+        // Conversion de Celsius vers Kelvin
+        return tCelsius + 273.15;
+    }
     public static void main(String[] args) {
        
         Scanner scanner = new Scanner(System.in);
@@ -21,12 +25,15 @@ public class TP1_convertisseur_PetitEnzo {
          
         System.out.println("La valeur reelle saisie est : " + valeurReelle);
        
+        System.out.print("Veuillez entrer une temperature en degres Celsius : ");
+        double temperatureCelsius = scanner.nextDouble();
         
+        System.out.println("La temperature en degres Celsius est : " + temperatureCelsius);
+          
+        double temperatureKelvin = temperatureCelsius + 273.15;
+        
+        System.out.println("La temperature en degres Kelvin est : " + temperatureKelvin);
         
         scanner.close();
-    }
 }
-        
-    
-    
-
+}
