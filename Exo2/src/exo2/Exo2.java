18b49b5f73902db2779be6b09cd222170af0b7f9
enzop <enzop@10.3.61.212>
2024-09-26 17:19:39 +0200

    Création d'un nouveau projet versionné

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -17,23 +17,23 @@ public class Exo2 {
      */
     public static void main(String[] args) {
       
-      //Declaration des variables
+     
 int nb;
-int result; // resultat
-int ind; //indice
-//nb=5;
+int result; 
+int ind; 
+
 Scanner sc = new Scanner(System.in);
        System.out.println("\n Entrer le nombre :");
-       nb=sc.nextInt(); // On demande a sc de donner le prochain entier// nombre dentiers a additionner
+       nb=sc.nextInt(); 
        
 result=0;
-// Addition des nb premiers entiers
+
 ind=1;
 while (ind <= nb) {
 result=result+ind;
 ind+=1; 
 }
-// Affichage du resultat
+
 System.out.println();
 System.out.println("La somme des "+ nb + "entiers est: "+result);
 }
