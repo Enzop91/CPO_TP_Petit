84aaa4ac9e0d89923010173380dfed5b7c86981a
enzop <enzop@10.3.61.212>
2024-10-23 11:15:11 +0200

    Création d'un nouveau projet versionné

--- TP2_Bieres_PetitEnzo/src/tp2_bieres_petitenzo/TP2_Bieres_PetitEnzo.java
+++ TP2_Bieres_PetitEnzo/src/tp2_bieres_petitenzo/TP2_Bieres_PetitEnzo.java
@@ -15,9 +15,24 @@ public class TP2_Bieres_PetitEnzo {
      */
     public static void main(String[] args) {
     
-        BouteilleBiere uneBiere = new BouteilleBiere() ;
-        uneBiere.nom = "Cuvée des trolls";
-        uneBiere.degreAlcool = 7.0f;
-
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",
+        7.0f ,"Dubuisson") ;
+        uneBiere.lireEtiquette();
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6f, "Abbaye de Leffe");
+        BouteilleBiere autreBiere1 = new BouteilleBiere("Guinness", 4.2f, "St. James's Gate");
+        BouteilleBiere autreBiere2 = new BouteilleBiere("Chimay Bleue", 9.0f, "Abbaye de Scourmont");
+        BouteilleBiere autreBiere3 = new BouteilleBiere("Leffe Blonde",8.8f,"Abbaye de Leffe");
+        autreBiere.lireEtiquette();
+        autreBiere1.lireEtiquette();
+        autreBiere2.lireEtiquette();
+        autreBiere3.lireEtiquette();
+        boolean resultat = autreBiere.Decapsuler();
+        System.out.println("Décapsulage réussi : " + resultat);
+        
+        System.out.println(uneBiere);
+        System.out.println(autreBiere);
     }
+   
+        
+        
  }
