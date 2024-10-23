7e46eac4b0586394a0bfeee10d8af8dc1beb2db9
enzop <enzop@10.3.61.212>
2024-10-23 11:16:08 +0200

    Création d'un nouveau projet versionné

--- TP2_Bieres_PetitEnzo/src/tp2_bieres_petitenzo/BouteilleBiere.java
+++ TP2_Bieres_PetitEnzo/src/tp2_bieres_petitenzo/BouteilleBiere.java
@@ -9,14 +9,40 @@ package tp2_bieres_petitenzo;
  * @author enzop
  */
 public class BouteilleBiere {
-    
+ 
     String nom;
     float degreAlcool;
     String brasserie;
-    boolean ouverte;
+    boolean ouverte;        
+
+public BouteilleBiere(String unNom, float unDegre, String
+uneBrasserie) {
+ nom = unNom;
+ degreAlcool = unDegre;
+ brasserie = uneBrasserie;
+ ouverte = false;
+}
 
     public void lireEtiquette() {
         System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degres)\nBrasserie : " + brasserie);
     }
+ 
+    public boolean Decapsuler() {
+        if (!ouverte) {
+            ouverte = true;
+            return true;
+        } else {
+            System.out.println("Erreur : bière déjà ouverte.");
+            return false;
+        }
+    }
+    @Override
+     public String toString() {
+        return "Nom : " + nom + "\n" +
+               "Degré d'alcool : " + degreAlcool + "%\n" +
+               "Brasserie : " + brasserie + "\n" +
+               "Ouverte : " + (ouverte ? "Oui" : "Non");
+    }
+
 }
 
